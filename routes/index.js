1ddfc731cbaa9f35c8cd8433fbe9192a540c09ea
Javier Diaz <coderdiaz@gmail.com>
2018-09-01 11:53:37 -0500

    feat: added route for publications

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET publication page */
+router.get('/forum/:slug', (req, res, next) => {
+  res.render('publication', {});
+});
+
 module.exports = router;
